09547c5ad58f597544eb04458811300a2efd2933
Wilton Graves <wiltongraves@gmail.com>
2014-06-13 20:23:56 -0400

    Added team page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get "about" => 'pages#about' #gives an about_path
+  get "team" => 'pages#team' #gives a team_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
