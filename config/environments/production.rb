801848721ed54eb15a38f89d109faff9c3035d25
Wilton Graves <wiltongraves@gmail.com>
2014-06-16 12:25:20 -0400

    Added Devise Users etc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #required for Heroku (I think for Devise)
+  config.action_mailer.default_url_options = { host: 'agentinvite.com' }
 end
